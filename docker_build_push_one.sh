b5c94f251e176e36fc74478de25c99164e37b718
andig91 <43803844+andig91@users.noreply.github.com>
2024-08-25 00:32:18 +0200

    speed up test builds without docker-registry upload

--- docker_build_push_one.sh
+++ docker_build_push_one.sh
@@ -8,17 +8,27 @@ then
 	exit
 fi
 
+if [ -z "$2" ]
+then
+	echo "Push to Docker-Registry and registry.gruber.live"
+else
+	echo "Push only to registry.gruber.live"
+fi
+
 if [ -d $1 ]; then
 	echo "Dir exists"
     name=$(echo "$d" | cut -d "/" -f 1)
-	docker build --no-cache --pull --progress=plain -t andi91/$1 $1
-	docker image tag andi91/$1:latest andi91/$1:$todaydate
-	docker push andi91/$1:latest
-	docker push andi91/$1:$todaydate
-	docker image tag andi91/$1:latest registry.gruber.live/andi91/$1:latest
-	docker image tag andi91/$1:latest registry.gruber.live/andi91/$1:$todaydate
+	docker build --no-cache --pull --progress=plain -t registry.gruber.live/andi91/$1 $1
+	docker image tag registry.gruber.live/andi91/$1:latest registry.gruber.live/andi91/$1:$todaydate
 	docker push registry.gruber.live/andi91/$1:latest
 	docker push registry.gruber.live/andi91/$1:$todaydate
+	if [ ! -z "$2" ]
+	then
+		docker image tag registry.gruber.live/andi91/$1:latest andi91/$1:latest
+		docker image tag registry.gruber.live/andi91/$1:latest andi91/$1:$todaydate
+		docker push andi91/$1:latest
+		docker push andi91/$1:$todaydate
+	fi
 else
 	echo "Dir not existing"
 fi
--- docker_build_push_one_multiarch.sh
+++ docker_build_push_one_multiarch.sh
@@ -8,6 +8,13 @@ then
 	exit
 fi
 
+if [ -z "$2" ]
+then
+	echo "Push to Docker-Registry and registry.gruber.live"
+else
+	echo "Push only to registry.gruber.live"
+fi
+
 if [ -f "$1/multiarch" ]
 then
 	architecture=$(sed -n 1p $1/multiarch)
@@ -24,10 +31,14 @@ fi
 if [ -d $1 ]; then
 	echo "Dir exists"
     name=$(echo "$d" | cut -d "/" -f 1)
-    docker buildx build  --platform $architecture -t andi91/$1:latest --push --no-cache $1
-    docker buildx build  --platform $architecture -t andi91/$1:$todaydate --push $1
-    docker buildx build  --platform $architecture -t registry.gruber.live/andi91/$1:latest --push $1
+    docker buildx build  --platform $architecture -t registry.gruber.live/andi91/$1:latest --push --no-cache $1
     docker buildx build  --platform $architecture -t registry.gruber.live/andi91/$1:$todaydate --push $1
+    if [ ! -z "$2" ]
+	then
+    	docker buildx build  --platform $architecture -t andi91/$1:latest --push $1
+    	docker buildx build  --platform $architecture -t andi91/$1:$todaydate --push $1
+	fi
+	
 	#docker build --no-cache --pull --progress=plain -t andi91/$1 $1
 	#docker image tag andi91/$1:latest andi91/$1:$todaydate
 	#docker push andi91/$1:latest
